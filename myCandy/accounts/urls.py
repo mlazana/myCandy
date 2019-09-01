f88eef309056f7df536e6763e92fe18438f4a3c4
mlazana <mariettalaz98@gmail.com>
2019-09-01 21:09:06 +0300

    create login form

--- myCandy/accounts/urls.py
+++ myCandy/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django import forms
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^login/$', LoginView, {'template_name': 'accounts/login.html'})
 ]
--- myCandy/accounts/views.py
+++ myCandy/accounts/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django import forms
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return render(request, 'accounts/login.html')
+    return render(request, 'registration/login.html')
