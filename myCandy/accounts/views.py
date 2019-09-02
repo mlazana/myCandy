36df98b6c0f2b37629f13588215fda667a9e13d1
mlazana <mariettalaz98@gmail.com>
2019-09-03 02:05:40 +0300

    add functionality to homePage form

--- myCandy/accounts/views.py
+++ myCandy/accounts/views.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 from django import forms
 from django.shortcuts import render, HttpResponse
+from .forms import valueForm
 
 def home(request):
-    return render(request, 'home.html',{})
+    if request.method == "POST":
+        form = valueForm(request.POST)
+        text = form['moneyValue'].value()
+        return  render(request, 'result.html',{'result':text})
+    else:
+        return render(request, 'home.html')
 
