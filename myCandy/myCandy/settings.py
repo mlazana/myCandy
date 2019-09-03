40066881f322e6f71404fc8428deb1ca71d13624
mlazana <mariettalaz98@gmail.com>
2019-09-03 13:35:29 +0300

    remove crispy forms

--- myCandy/myCandy/settings.py
+++ myCandy/myCandy/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +124,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/home'
